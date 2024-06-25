7043a1cc5690e9d4cb88caf4b494549ac3e7290a
Joanna Lim <m210068@e.ntu.edu.sg>
2024-06-25 17:09:06 +0800

    transfer money feature

--- src/main/java/dev/interns/BankDemo/controller/TransactionController.java
+++ src/main/java/dev/interns/BankDemo/controller/TransactionController.java
@@ -1,4 +1,40 @@
 package dev.interns.BankDemo.controller;
 
+import dev.interns.BankDemo.entity.Transaction;
+import dev.interns.BankDemo.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/transactions")
 public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    @Autowired
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Transaction>> getAllTransactions() {
+        List<Transaction> transactions = transactionService.getAllTransactions();
+        return new ResponseEntity<>(transactions, HttpStatus.OK);
+    }
+
+    @PostMapping("/transfer")
+    public ResponseEntity<Optional<Transaction>> transferMoney(
+            @RequestParam Long fromBankAccNum,
+            @RequestParam Long toBankAccNum,
+            @RequestParam Double amount) {
+        Optional<Transaction> transaction = transactionService.transferMoney(fromBankAccNum, toBankAccNum, amount);
+        return transaction.isPresent()
+                ? new ResponseEntity<>(transaction, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
 }
--- src/main/java/dev/interns/BankDemo/service/TransactionService.java
+++ src/main/java/dev/interns/BankDemo/service/TransactionService.java
@@ -1,4 +1,59 @@
 package dev.interns.BankDemo.service;
 
+import dev.interns.BankDemo.entity.BankAccount;
+import dev.interns.BankDemo.entity.Transaction;
+import dev.interns.BankDemo.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TransactionService {
+
+    private final TransactionRepository transactionRepository;
+    private final BankAccountService bankAccountService;
+
+    @Autowired
+    public TransactionService(TransactionRepository transactionRepository, BankAccountService bankAccountService) {
+        this.transactionRepository = transactionRepository;
+        this.bankAccountService = bankAccountService;
+    }
+
+    public List<Transaction> getAllTransactions() {
+        return transactionRepository.findAll();
+    }
+
+    //TODO get transactions filtered by accNum (both sent and received)
+
+    //TODO get transactions filtered by customerID (all accts linked to them)
+
+
+    public Optional<Transaction> transferMoney(Long fromBankAccNum, Long toBankAccNum, Double amount) {
+        try {
+            // try to remove balance from the sender's account
+            Optional<BankAccount> fromAccountOpt = bankAccountService.removeBalance(fromBankAccNum, amount);
+
+            if (fromAccountOpt.isPresent()) {
+                Optional<BankAccount> toAccountOpt = bankAccountService.addBalance(toBankAccNum, amount);
+
+                if (toAccountOpt.isPresent()) {
+
+                    Transaction transaction = new Transaction();
+                    transaction.setFromBankAccNum(fromBankAccNum);
+                    transaction.setToBankAccNum(toBankAccNum);
+                    transaction.setAmount(amount);
+                    transaction.setDatetime(LocalDateTime.now());
+                    transactionRepository.save(transaction);
+
+                    return Optional.of(transaction);
+                }
+            }
+        } catch (IllegalArgumentException e) {
+            return Optional.empty();
+        }
+        return Optional.empty();
+    }
 }
