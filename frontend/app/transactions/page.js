b9ecbef19477f4cafe6e5845fb8387573d1a255b
ilovedangomilk <123913697+ilovedangomilk@users.noreply.github.com>
2024-07-07 17:57:16 +0800

    authentication validation for transaction and trasnfer

--- frontend/app/transactions/page.js
+++ frontend/app/transactions/page.js
@@ -1,12 +1,19 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Transactions = () => {
   const [fromBankAccNum, setFromBankAccNum] = useState("");
   const [transactions, setTransactions] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [userid, setUserid] = useState('');
+
+  useEffect(() => {
+    setUserid(localStorage.getItem('userid'));
+  }, []);
 
   const handleSearch = (e) => {
     e.preventDefault();
@@ -36,71 +43,78 @@ const Transactions = () => {
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold tracking-tight my-4">Transaction History</h1>
-      <form onSubmit={handleSearch} className="mb-4">
-        <label className="block text-gray-700 mb-2">
-          Enter Bank Account Number
-        </label>
-        <input
-          type="text"
-          value={fromBankAccNum}
-          onChange={(e) => setFromBankAccNum(e.target.value)}
-          className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 mb-2"
-          placeholder="Enter bank account number"
-          required
-        />
-        <button
-          type="submit"
-          className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-200"
-        >
-          Search
-        </button>
-      </form>
-      {loading && <div>Loading...</div>}
-      {error && <div>Error: {error.message}</div>}
-      {transactions.length > 0 && (
-        <div className="overflow-x-auto">
-          <table className="min-w-full bg-white">
-            <thead>
-              <tr>
-                <th className="py-2 px-4 text-left bg-gray-100">Date</th>
-                <th className="py-2 px-4 text-left bg-gray-100">
-                  Bank Account
-                </th>
-                <th className="py-2 px-4 text-left bg-gray-100">Amount</th>
-              </tr>
-            </thead>
-            <tbody>
-              {transactions.map((transaction) => (
-                <tr key={transaction.transactionId} className="border-b">
-                  <td className="py-2 px-4 text-left">
-                    {formatDate(transaction.datetime)}
-                  </td>
-                  <td className="py-2 px-4 text-left">
-                  {transaction.fromBankAccNum.toString() === fromBankAccNum
-                      ? "To: " + transaction.toBankAccNum
-                      : "From: " + transaction.fromBankAccNum}
-                  </td>
-                  <td
-                    className={`py-2 px-4 text-left ${
-                      transaction.fromBankAccNum.toString() === fromBankAccNum
-                        ? "text-red-500"
-                        : "text-green-500"
-                    }`}
-                  >
-                    {transaction.fromBankAccNum.toString() === fromBankAccNum
-                      ? "-"
-                      : "+"}
-                    ${Math.abs(transaction.amount).toFixed(2)}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+    <>
+      {userid ? (
+        <div className="container mx-auto p-4">
+          <h1 className="text-3xl font-bold tracking-tight my-4">Transaction History</h1>
+          <form onSubmit={handleSearch} className="mb-4">
+            <label className="block text-gray-700 mb-2">
+              Enter Bank Account Number
+            </label>
+            <input
+              type="text"
+              value={fromBankAccNum}
+              onChange={(e) => setFromBankAccNum(e.target.value)}
+              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 mb-2"
+              placeholder="Enter bank account number"
+              required
+            />
+            <button
+              type="submit"
+              className="bg-black text-white py-2 px-4 rounded-md hover:bg-zinc-800 transition duration-200"
+            >
+              Search
+            </button>
+          </form>
+          {loading && <div>Loading...</div>}
+          {error && <div>Error: {error.message}</div>}
+          {transactions.length > 0 && (
+            <div className="overflow-x-auto">
+              <table className="min-w-full bg-white">
+                <thead>
+                  <tr>
+                    <th className="py-2 px-4 text-left bg-gray-100">Date</th>
+                    <th className="py-2 px-4 text-left bg-gray-100">Bank Account</th>
+                    <th className="py-2 px-4 text-left bg-gray-100">Amount</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {transactions.map((transaction) => (
+                    <tr key={transaction.transactionId} className="border-b">
+                      <td className="py-2 px-4 text-left">{formatDate(transaction.datetime)}</td>
+                      <td className="py-2 px-4 text-left">
+                        {transaction.fromBankAccNum.toString() === fromBankAccNum
+                          ? "To: " + transaction.toBankAccNum
+                          : "From: " + transaction.fromBankAccNum}
+                      </td>
+                      <td
+                        className={`py-2 px-4 text-left ${
+                          transaction.fromBankAccNum.toString() === fromBankAccNum
+                            ? "text-red-500"
+                            : "text-green-500"
+                        }`}
+                      >
+                        {transaction.fromBankAccNum.toString() === fromBankAccNum
+                          ? "-"
+                          : "+"}
+                        ${Math.abs(transaction.amount).toFixed(2)}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
+      ) : (
+        <div className="flex flex-col justify-center items-center text-center mt-52">
+          <p className="text-4xl font-semibold"> Please sign in to view your account</p>
+          <Button asChild className="mx-2 w-2/5 text-2xl p-8 mt-6" size="lg">
+            <Link href="/signin">Sign in</Link>
+          </Button>
         </div>
       )}
-    </div>
+    </>
   );
 };
 
--- frontend/app/transfer/page.js
+++ frontend/app/transfer/page.js
@@ -1,6 +1,8 @@
 "use client";
 
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Transfer = () => {
   const recipientRef = useRef();
@@ -9,6 +11,11 @@ const Transfer = () => {
   const [fromBankAccNum, setFromBankAccNum] = useState(""); // Add state for fromBankAccNum
   const [error, setError] = useState(null);
   const [success, setSuccess] = useState(null);
+  const [userid, setUserid] = useState('');
+
+  useEffect(() => {
+    setUserid(localStorage.getItem('userid'));
+  }, []);
 
   const handleTransfer = (e) => {
     e.preventDefault();
@@ -48,59 +55,70 @@ const Transfer = () => {
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold tracking-tight mt-4 mb-7">Transfer to Other Accounts</h1>
-      <form onSubmit={handleTransfer} className="max-w-md mx-auto bg-white p-8 shadow-md rounded-md">
-        <div className="mb-4">
-          <label className="block text-gray-700">From Account</label>
-          <input
-            type="text"
-            value={fromBankAccNum}
-            onChange={(e) => setFromBankAccNum(e.target.value)}
-            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            placeholder="Enter your account number"
-            required
-          />
+    <>
+      {userid ? (
+        <div className="container mx-auto p-4">
+          <h1 className="text-3xl font-bold tracking-tight mt-4 mb-7">Transfer to Other Accounts</h1>
+          <form onSubmit={handleTransfer} className="max-w-md mx-auto bg-white p-8 shadow-md rounded-md">
+            <div className="mb-4">
+              <label className="block text-gray-700">From Account</label>
+              <input
+                type="text"
+                value={fromBankAccNum}
+                onChange={(e) => setFromBankAccNum(e.target.value)}
+                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="Enter your account number"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700">Recipient Account</label>
+              <input
+                type="text"
+                ref={recipientRef}
+                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="Enter recipient account"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700">Amount</label>
+              <input
+                type="number"
+                ref={amountRef}
+                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="Enter amount"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700">Description</label>
+              <textarea
+                ref={descriptionRef}
+                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="Enter description"
+                rows="3"
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-black text-white py-2 rounded-md hover:bg-zinc-800 transition duration-200"
+            >
+              Transfer
+            </button>
+          </form>
+          {error && <div className="text-red-500 mt-4 justify-center items-center flex text-xl font-semibold">{error}</div>}
+          {success && <div className="text-green-500 mt-4 justify-center items-center flex text-xl font-semibold">{success}</div>}
         </div>
-        <div className="mb-4">
-          <label className="block text-gray-700">Recipient Account</label>
-          <input
-            type="text"
-            ref={recipientRef}
-            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            placeholder="Enter recipient account"
-            required
-          />
+      ) : (
+        <div className="flex flex-col justify-center items-center text-center mt-52">
+          <p className="text-4xl font-semibold"> Please sign in to view account</p>
+          <Button asChild className="mx-2 w-2/5 text-2xl p-8 mt-6" size="lg">
+            <Link href="/signin">Sign in</Link>
+          </Button>
         </div>
-        <div className="mb-4">
-          <label className="block text-gray-700">Amount</label>
-          <input
-            type="number"
-            ref={amountRef}
-            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            placeholder="Enter amount"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700">Description</label>
-          <textarea
-            ref={descriptionRef}
-            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            placeholder="Enter description"
-            rows="3"
-          />
-        </div>
-        <button
-          type="submit"
-          className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition duration-200"
-        >
-          Transfer
-        </button>
-      </form>
-      {error && <div className="text-red-500 mt-4 justify-center items-center flex text-xl font-semibold">{error}</div>}
-      {success && <div className="text-green-500 mt-4 justify-center items-center flex text-xl font-semibold">{success}</div>}
-    </div>
+      )}
+    </>
   );
 };
 
